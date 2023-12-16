a41003bc671ec5b05015941235ccd43fe8f314d0
mohammad ghorashi <mohammadghorashi81@gmail.com>
2023-12-16 19:35:56 +0330

    fixed bug and connection to pgadmin or postgres

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list_blog, name='list_blog'),
+    path('', views.blogs, name='list_blog'),
     path('create/', views.create, name='create_blog'),
     path('edit/<str:name>/', views.edit, name='edit_blog'),
     path('delete/<str:name>/', views.delete, name='delete_blog'),
--- config/settings.py
+++ config/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'book',
+        'USER': 'postgres',
+        'PASSWORD': 'mohammad',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
