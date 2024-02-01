db757194fe86629e16b1e2fd32812c8ef5f5deff
mohammad ghorashi <mohammadghorashi81@gmail.com>
2024-02-01 22:38:01 +0330

    add model comment of book

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Bookstore
-this projecct for bookstore
-    1 - buy book 
-    2 - make blog
-    3 - comment for book and blog
+this projecct for bookstore \
+    1 - Book  
+    2 - Blog \
+    3 - Comment for book and blog
 
--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class Book(models.Model):
@@ -6,6 +7,14 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.PositiveIntegerField()
-    photo = models.ImageField(upload_to='book/cover/%Y/%m', blank=True, null=True)
+    photo = models.ImageField(upload_to='book/cover/%Y/%m')
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comment_author')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='comment_author')
+    text = models.TextField()
+    published = models.BooleanField(default=False)
+    datetime_created = models.DateTimeField(auto_now_add=True)
