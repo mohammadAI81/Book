3e59347195d1fbfd408d05361d2094b6a1a73fd6
mohammad ghorashi <mohammadghorashi81@gmail.com>
2024-02-29 00:31:22 +0330

    add admin panel of shop app

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CartItem, Cart
+
+
+class CartItemOfCart(admin.StackedInline):
+    model = CartItem
+    fields = ['product', 'number']
+    extra = 2
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'unit_price', 'number', )
+    list_editable = ('number', 'product', )
+    list_per_page = 25
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('customer', )
+    inlines = [
+        CartItemOfCart,
+    ]
+
